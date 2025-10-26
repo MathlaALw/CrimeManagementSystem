4ac16a51cfc6191d73bca11244344354c8d61982
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-10-26 15:34:21 +0400

    create EvidenceDtos

---
+++ DTOs/CreateImageEvidenceDto.cs
@@ -0,0 +1,13 @@
+﻿using Crime_Management_System.Models;
+using System.ComponentModel.DataAnnotations;
+namespace Crime_Management_System.DTOs
+{
+    public class CreateImageEvidenceDto
+    {
+        [Required]
+         public int CaseId { get; set; }
+        [Required] 
+        public IFormFile Image { get; set; } = null!;
+        public  string? Remarks { get; set; }
+    }
+}
