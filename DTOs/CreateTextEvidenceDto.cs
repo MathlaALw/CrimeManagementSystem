245d906a6c817fe77cd5b612abbe1a58680215a9
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-10-26 15:30:55 +0400

    CreateTextEvidenceDto

---
+++ DTOs/CreateTextEvidenceDto.cs
@@ -0,0 +1,14 @@
+﻿using Crime_Management_System.Models;
+using System.ComponentModel.DataAnnotations;
+namespace Crime_Management_System.DTOs
+{
+    public class CreateTextEvidenceDto
+    {
+        [Required]
+        public int CaseId { get; set; }
+
+        [Required] 
+         public string TextContent { get; set; } = string.Empty;
+         public string? Remarks { get; set; }
+    }
+}
