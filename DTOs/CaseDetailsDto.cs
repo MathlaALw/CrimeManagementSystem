6aa4ce25fb4bef62d0ac3c87241ece0163addea5
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-10-26 15:18:10 +0400

    create CaseDetailsDto

---
+++ DTOs/CaseDetailsDto.cs
@@ -0,0 +1,25 @@
+﻿using Crime_Management_System.Models;
+
+namespace Crime_Management_System.DTOs
+{
+    public class CaseDetailsDto
+    {
+         public int Id { get; set; }
+         public string CaseNumber { get; set; }
+          public  string Name { get; set; }
+
+         public string? Description {get; set; }
+         public string? AreaCity { get; set; }
+         public string? CaseType { get; set; }
+         public  CaseStatus Status { get; set; }
+         public  ClearanceLevel AuthorizationLevel { get; set; }
+         public  string CreatedBy { get; set; }
+         public  DateTime CreatedAt { get; set; }
+         public string? ReportedBy { get; set; }
+        public int Assignees { get; set; }
+         public int Evidences { get; set; }
+        public  int Suspects { get; set; }
+        public int Victims { get; set; }
+        public  int Witnesses { get; set; }
+    }
+}
