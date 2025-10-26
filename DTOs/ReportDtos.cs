66ed2402195678cf744c413ca2702bb56027333f
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-10-26 15:21:40 +0400

    create ReportDtos

---
+++ DTOs/ReportDtos.cs
@@ -0,0 +1,12 @@
+﻿namespace Crime_Management_System.DTOs
+{
+    public class ReportDtos
+    {
+       public string? Title { get; set; }
+       public string? Description { get; set; }
+        public string? AreaCit { get; set; }
+        decimal? Latitude { get; set; }
+        decimal? Longitude { get; set; }
+        int? ReportedByUserId { get; set; }
+    }
+}
