b00595c5c8197e1f3aec3d8a3ea20fcad5ac88e0
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-10-26 14:59:09 +0400

    restructure project files

---
+++ Repos/GenericRepo.cs
@@ -0,0 +1,11 @@
+﻿using Crime_Management_System.Data;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace Crime_Management_System.Repos
+{
+    public class GenericRepo
+    {
+
+    
+    }
+}
---
+++ Repos/IGenericRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace Crime_Management_System.Repos
+{
+    public interface IGenericRepo
+    {
+     
+    }
+}
